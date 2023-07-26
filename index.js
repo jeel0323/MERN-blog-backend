8649b61b8114d0b92b1390d74cc172f4ff9a66e0
Jeel <jeel4580patel@gmail.com>
2023-07-26 19:22:02 +0530

    Final working model done 1

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const Post = require("./models/Post");
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
-mongoose.connect(
-  "mongodb+srv://jeel:jeel4580@jeel.wbjbv6r.mongodb.net/");
+mongoose.connect("mongodb+srv://jeel:jeel4580@jeel.wbjbv6r.mongodb.net/");
 
 app.post("/register", async (req, res) => {
   try {
@@ -76,30 +76,35 @@ app.get("/profile", (req, res) => {
   }
 });
 
-// app.post("/post", upload.any(), (req, res) => {
-//   const {originalname,path} = req.files;
-//   const parts = originalname.split('.');
-//   const ext = parts[parts.length -1];
-//   const newPath = path+'.'+ext;
-//   fs.renameSync(path, newPath);
-//   res.json({ files: req.files });
-// });
+app.get("/post", async (req, res) => {
+  const posts = await Post.find()
+    .populate("author", ["username"])
+    .sort({ createdAt: -1 })
+    .limit(20);
+  res.json(posts);
+});
+
 app.post("/post", upload.single("files"), async (req, res) => {
   try {
     const { originalname, path } = req.file;
-    const parts = originalname.split('.');
+    const parts = originalname.split(".");
     const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
+    const newPath = path + "." + ext;
     fs.renameSync(path, newPath);
 
-    const { title, summary, content } = req.body;
-    const postDoc = await Post.create({
-      title,
-      summary,
-      content,
-      cover: newPath,
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { title, summary, content } = req.body;
+      const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+        author: info.id,
+      });
+      res.json(postDoc);
     });
-    res.json(postDoc);
   } catch (err) {
     console.error("Error:", err);
     res.status(500).json({ error: "Failed to process uploaded files" });
@@ -108,6 +113,48 @@ app.post("/post", upload.single("files"), async (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
+app.put("/post", upload.single("files"), async (req, res) => {
+  try {
+    let newPath = null;
+    if (req.file) {
+      const { originalname, path } = req.file;
+      const parts = originalname.split(".");
+      const ext = parts[parts.length - 1];
+      const newPath = path + "." + ext;
+      fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { title, summary, content, id } = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor =
+        JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json("You are not the author");
+      }
+
+      await postDoc.updateOne({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+      return res.json(postDoc);
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ error: "Failed to process uploaded files" });
+  }
 });
 
 app.listen(4000, () => {
